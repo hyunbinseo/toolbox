72c7be620dcded48bc59337062ba681eec9659d3
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-03-16 13:53:31 +0900

    feat: press a to select textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,39 +6,49 @@
 
 	const presets: Array<{ description: string; fn: (text: string) => string }> = [
 		{
-			description: 'Remove all blank lines',
-			fn: (text) => text.replaceAll(/\n{2,}/g, '\n')
-		},
-		{
 			description: 'Remove duplicate blank lines',
 			fn: (text) => text.replaceAll(/\n{3,}/g, '\n\n')
+		},
+		{
+			description: 'Remove all blank lines',
+			fn: (text) => text.replaceAll(/\n{2,}/g, '\n')
 		}
 	];
 
-	const selectRadio = async ({ key }: KeyboardEvent) => {
-		if (!/\d/.test(key)) return;
-		const number = Number(key);
-		const radioButton = radioButtonList.querySelectorAll('input[type="radio"]')[number - 1];
-		if (!radioButton) return;
-		checkedToolValue = number - 1;
-		setTimeout(() => textarea.focus());
+	const pause = () => new Promise((resolve) => setTimeout(resolve));
+
+	const handleKeyboardShortcut = async ({ key, keyCode }: KeyboardEvent) => {
+		if (document.activeElement === textarea) return;
+		if (keyCode === 65) {
+			await pause();
+			textarea.scrollTop = 0;
+			textarea.select();
+		}
+		if (/\d/.test(key)) {
+			const number = Number(key);
+			const radioButton = radioButtonList.querySelectorAll('input[type="radio"]')[number - 1];
+			if (!radioButton) return;
+			checkedToolValue = number - 1;
+			await pause();
+			textarea.focus();
+		}
 	};
 
-	const blurTextarea = async (e: KeyboardEvent) => {
-		if (e.key === 'Escape') (document.activeElement as HTMLElement)?.blur();
+	const blurTextarea = async ({ key }: KeyboardEvent) => {
+		if (key === 'Escape') (document.activeElement as HTMLElement)?.blur();
 	};
 
 	const pastedTextArea = async (e: Event) => {
 		const tool = presets[checkedToolValue];
 		if (!tool) return;
-		await new Promise((resolve) => setTimeout(resolve));
+		await pause();
 		const pasted = (e.target as HTMLTextAreaElement).value;
 		textarea.value = tool.fn(trimText ? pasted.trim() : pasted);
 		textarea.select();
 	};
 </script>
 
-<svelte:window on:keydown={selectRadio} />
+<svelte:window on:keydown={handleKeyboardShortcut} />
 
 <ol bind:this={radioButtonList}>
 	{#each presets as { description }, index}
