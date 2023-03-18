19557e38584e1da8c53db01e9f55fb15c92b4886
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-03-18 14:31:10 +0900

    chore: global navigation bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const paths = ['text'];
+
+	$: currentPath = $page.url.pathname.substring(1);
+	$: remainingPaths = paths.filter((path) => path !== currentPath);
+</script>
+
+<nav>
+	<span>{currentPath || 'Home'}</span>
+	{#each remainingPaths as path (path)}
+		<a href="/{path}">{path}</a>
+	{/each}
+</nav>
+
+<hr />
+
+<slot />
+
+<hr />
+
+<footer>
+	Tools by <a href="https://github.com/hyunbinseo/">Hyunbin</a>
+	(<a href="https://github.com/hyunbinseo/tools">Source</a>)
+</footer>
+
+<style>
+	hr {
+		margin: 1rem 0;
+	}
+	nav {
+		display: flex;
+		gap: 0.5rem;
+	}
+	nav > span {
+		font-weight: bold;
+		margin-right: 0.5rem;
+	}
+	nav > span,
+	nav a {
+		text-transform: capitalize;
+	}
+</style>
