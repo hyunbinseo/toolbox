87eb584c85aa7faaed052f4f124799df5d49071a
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-01 10:48:06 +0900

    chore: add delibox tracking url

--- src/routes/(tool)/parcel/+page.svelte
+++ src/routes/(tool)/parcel/+page.svelte
@@ -13,7 +13,8 @@
 		한진택배:
 			'https://www.hanjin.co.kr/kor/CMS/DeliveryMgr/WaybillResult.do?mCode=MN038&schLang=KR&wblnumText2=',
 		천일택배: 'https://www.chunil.co.kr/HTrace/HTrace.jsp?transNo=',
-		큐익스프레스: 'https://www.qxpress.net/Customer/PopupTraceParcels?TrackingNo='
+		큐익스프레스: 'https://www.qxpress.net/Customer/PopupTraceParcels?TrackingNo=',
+		딜리박스: 'https://www.dbox.co.kr/delivery.php?laasInvcNo='
 	} satisfies Record<string, `https://${string}`>;
 
 	let urls: string[] = [];
