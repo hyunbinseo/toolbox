e77ab65c68e92bc623b478f18c98dee031aaeb8c
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-01 10:47:34 +0900

    chore: bump eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: tseslint.parser
+			}
+		}
+	},
+	{ ignores: ['.svelte-kit/', 'build/', 'dist/', 'drizzle/'] },
+	{
+		rules: {
+			'no-console': ['error', { allow: ['warn', 'error'] }],
+			'no-restricted-imports': ['error', 'assert', 'node:assert']
+		}
+	}
+);
